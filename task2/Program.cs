86ed381210139b2e5cd39e06016847406fdd5668
Dmitrii <kuzneczovdv@gmail.com>
2024-01-29 18:12:31 +0300

    task2

---
+++ task2/Program.cs
@@ -0,0 +1,11 @@
+﻿public class Task2
+{
+    public static void Main(string[] args)
+    {
+        string input = "DaStrEam!-";
+        string result = input.ToLower();
+
+        Console.WriteLine(result);
+    }
+}
+
