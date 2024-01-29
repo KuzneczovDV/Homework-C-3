846a48fa830308c0e3f8b5701104aa2f316f2287
Dmitrii <kuzneczovdv@gmail.com>
2024-01-29 18:44:14 +0300

    task4

---
+++ task4/Program.cs
@@ -0,0 +1,16 @@
+﻿public class Task4
+{
+    public static void Main(string[] args)
+    {
+        string input = "Hello my little friend !";
+        string result = ReverseWords(input);
+       
+        Console.WriteLine(result);
+    }
+    public static string ReverseWords(string str)
+    {
+        string[] words = str.Split(' ');
+        Array.Reverse(words);
+        return string.Join(" ", words);
+    }
+}
